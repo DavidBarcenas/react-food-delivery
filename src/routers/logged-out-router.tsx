2a6efea8ae43abff93355443eebabedfacb7efdf
David Barcenas <daveepro@outlook.com>
2022-03-18 15:18:21 -0600

    create logged out browser router

--- src/routers/logged-out-router.tsx
+++ src/routers/logged-out-router.tsx
@@ -1,14 +1,16 @@
-import {isLoggedInVar} from '../apollo';
+import {BrowserRouter, Route, Routes} from 'react-router-dom';
+import Login from '../pages/login';
+import Signup from '../pages/signup';
 
 function LoggedOutRouter() {
-  function onClick() {
-    isLoggedInVar(true);
-  }
   return (
-    <div>
-      <h1>LoggedInRouter</h1>
-      <button onClick={onClick}>Click to login</button>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Login />} />
+        <Route path='signup' element={<Signup />} />
+        <Route path='*' element={<h1>Upps! 404</h1>} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
