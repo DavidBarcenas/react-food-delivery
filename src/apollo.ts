297f89453968a26a85729fdd8f6a8ba1c881c80e
David Barcenas <daveepro@outlook.com>
2022-03-21 17:00:56 -0600

    configure apollo to send token in every request

--- src/apollo.ts
+++ src/apollo.ts
@@ -1,12 +1,26 @@
-import {ApolloClient, InMemoryCache, makeVar} from '@apollo/client';
+import {ApolloClient, createHttpLink, InMemoryCache, makeVar} from '@apollo/client';
+import {setContext} from '@apollo/client/link/context';
 import {LOCALSTORAGE_TOKEN} from './constants';
 
 const token = localStorage.getItem(LOCALSTORAGE_TOKEN);
 export const isLoggedInVar = makeVar(!!token);
 export const authToken = makeVar(token);
 
-export const client = new ApolloClient({
+const httpLink = createHttpLink({
   uri: 'http://localhost:4000/graphql',
+});
+
+const authLink = setContext((_, {headers}) => {
+  return {
+    headers: {
+      ...headers,
+      authorization: authToken() || '',
+    },
+  };
+});
+
+export const client = new ApolloClient({
+  link: authLink.concat(httpLink),
   cache: new InMemoryCache({
     typePolicies: {
       Query: {
--- src/routers/logged-in-router.tsx
+++ src/routers/logged-in-router.tsx
@@ -1,12 +1,32 @@
+import {gql, useQuery} from '@apollo/client';
 import {isLoggedInVar} from '../apollo';
+import {meQuery} from '../types/meQuery';
+
+const ME_QUERY = gql`
+  query meQuery {
+    me {
+      id
+      email
+      role
+      emailVerified
+    }
+  }
+`;
 
 function LoggedInRouter() {
+  const {data, loading, error} = useQuery<meQuery>(ME_QUERY);
+
   function onClick() {
     isLoggedInVar(false);
   }
+
+  if (!data || loading || error) {
+    return <div>Loading...</div>;
+  }
+
   return (
     <div>
-      <h1>LoggedOutRouter</h1>
+      <h1>{data.me?.role}</h1>
       <button onClick={onClick}>Click to logout</button>
     </div>
   );
