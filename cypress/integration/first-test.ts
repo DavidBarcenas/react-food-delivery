41be608cc3f79c8e0ea8a756129d7b72362bb074
David Barcenas <daveepro@outlook.com>
2022-04-01 11:05:09 -0600

    finalize login page e2e test

--- cypress/integration/first-test.ts
+++ cypress/integration/first-test.ts
@@ -2,31 +2,41 @@ describe('login', () => {
   const emailInput = 'input[name=email]';
   const passwordInput = 'input[name=password]';
   const submitButton = 'button[type=submit]';
+  const user = cy;
 
   it('should go to login page', () => {
-    cy.visit('http://localhost:3000').title().should('eq', 'Iniciar sesión | Food Delivery');
-  });
-
-  it('can fill form', () => {
-    cy.visit('/')
-      .get(emailInput)
-      .type('davidbarcenas@gmail.com')
-      .get(passwordInput)
-      .type('123456')
-      .get(submitButton);
+    user.visit('http://localhost:3000').title().should('eq', 'Iniciar sesión | Food Delivery');
   });
 
   it('can see email / password validation errors', () => {
-    cy.visit('/')
+    user
+      .visit('/')
       .get(emailInput)
       .type('fail@mail')
       .get(passwordInput)
       .type('123')
       .get(submitButton)
-      .click()
-      .get(':nth-child(1) > .block')
-      .should('have.text', 'Ingresa un correo válido')
-      .get(':nth-child(2) > .block')
+      .click();
+    user.findAllByRole('alert').eq(0).should('have.text', 'Ingresa un correo válido');
+    user.get(emailInput).clear();
+    user.findAllByRole('alert').eq(0).should('have.text', 'El correo es requerido');
+    user
+      .findAllByRole('alert')
+      .eq(1)
       .should('have.text', 'La contraseña debe tener al menos 6 caracteres');
+    user.get(passwordInput).clear();
+    user.findAllByRole('alert').eq(1).should('have.text', 'La contraseña es requerida');
+  });
+
+  it('get the token and access', () => {
+    user
+      .visit('/')
+      .get(emailInput)
+      .type('test@mail.com')
+      .get(passwordInput)
+      .type('secret')
+      .get(submitButton)
+      .click();
+    user.window().its('localStorage.fd-token').should('be.a', 'string');
   });
 });
