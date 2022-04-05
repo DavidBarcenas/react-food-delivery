f059f65b69be55a243d32c502448dd727e1b7b02
David Barcenas <daveepro@outlook.com>
2022-04-04 19:05:47 -0500

    get my restaurants and add types

--- src/pages/owner/my-restaurants.tsx
+++ src/pages/owner/my-restaurants.tsx
@@ -1,5 +1,23 @@
+import {gql, useQuery} from '@apollo/client';
+import {RESTAURANT_FRAGMENT} from '../../fragments';
+import {myRestaurants} from '../../types/myRestaurants';
+
+const MY_RESTAURANTS_QUERY = gql`
+  query myRestaurants {
+    myRestaurants {
+      ok
+      error
+      restaurants {
+        ...RestaurantFragment
+      }
+    }
+  }
+  ${RESTAURANT_FRAGMENT}
+`;
+
 function MyRestaurants() {
-  return <div>MyRestaurants</div>;
+  const {data} = useQuery<myRestaurants>(MY_RESTAURANTS_QUERY);
+  return <div></div>;
 }
 
 export default MyRestaurants;
