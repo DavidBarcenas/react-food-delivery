fa82f4b0f0526403a894efcb7e34aa31ac61a8c0
David Barcenas <daveepro@outlook.com>
2022-04-04 19:25:50 -0500

    add design to my restaurants page

--- src/pages/owner/my-restaurants.tsx
+++ src/pages/owner/my-restaurants.tsx
@@ -1,4 +1,7 @@
 import {gql, useQuery} from '@apollo/client';
+import {Link, useNavigate} from 'react-router-dom';
+import Button from '../../components/button';
+import RestaurantsGrid from '../../components/restaurants-grid';
 import {RESTAURANT_FRAGMENT} from '../../fragments';
 import {myRestaurants} from '../../types/myRestaurants';
 
@@ -16,8 +19,33 @@ const MY_RESTAURANTS_QUERY = gql`
 `;
 
 function MyRestaurants() {
+  const navigate = useNavigate();
   const {data} = useQuery<myRestaurants>(MY_RESTAURANTS_QUERY);
-  return <div></div>;
+
+  function goToAddRestaurant(): void {
+    navigate('/add-restaurant');
+  }
+
+  return (
+    <section>
+      <header className='flex justify-between p-10'>
+        <h2 className='text-2xl font-medium'>Mis restaurantes</h2>
+        <Button text='Agregar' mode='normal' onClick={goToAddRestaurant} />
+      </header>
+      <div className='px-10'>
+        {data?.myRestaurants.ok && data.myRestaurants.restaurants.length === 0 ? (
+          <div className='text-center'>
+            <p>No tienes restaurantes</p>{' '}
+            <Link to='/add-restaurant' className='ml-1 text-lime-500 hover:underline'>
+              Agregar un restaurante
+            </Link>
+          </div>
+        ) : (
+          <RestaurantsGrid restaurants={data?.myRestaurants.restaurants} />
+        )}
+      </div>
+    </section>
+  );
 }
 
 export default MyRestaurants;
