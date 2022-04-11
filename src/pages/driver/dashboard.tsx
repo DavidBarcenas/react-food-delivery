7680d08f7114b464e5419dc1cce8c8eb8629a238
David Barcenas <daveepro@outlook.com>
2022-04-10 19:33:10 -0500

    get driver coords

--- src/pages/driver/dashboard.tsx
+++ src/pages/driver/dashboard.tsx
@@ -1,22 +1,53 @@
 import ReactMapboxGl, {Layer, Feature} from 'react-mapbox-gl';
-import 'mapbox-gl/dist/mapbox-gl.css';
+import {useEffect, useState} from 'react';
+
+interface Coords {
+  lat: number;
+  lng: number;
+}
 
 const Map = ReactMapboxGl({
   accessToken: process.env.REACT_APP_MAP_KEY || '',
 });
 
 function Dashboard() {
+  const [coords, setCoords] = useState<Coords>({lat: 0, lng: 0});
+
+  useEffect(() => {
+    navigator.geolocation.watchPosition(onSuccess, onError, {
+      enableHighAccuracy: true,
+    });
+  }, []);
+
+  function onSuccess({coords}: GeolocationPosition) {
+    setCoords({lat: coords.latitude, lng: coords.longitude});
+  }
+
+  function onError(error: GeolocationPositionError) {
+    console.log(error);
+  }
+
   return (
     <div>
       <div>
         <Map
           style='mapbox://styles/mapbox/streets-v9'
+          center={[coords.lng, coords.lat]}
+          zoom={[15]}
           containerStyle={{
-            height: '100vh',
+            height: '90vh',
             width: '100vw',
           }}>
-          <Layer type='symbol' id='marker' layout={{'icon-image': 'marker-15'}}>
-            <Feature coordinates={[-0.481747846041145, 51.3233379650232]} />
+          <Layer
+            type='circle'
+            id='marker'
+            paint={{
+              'circle-stroke-width': 0,
+              'circle-radius': 10,
+              'circle-blur': 0.15,
+              'circle-color': '#abdbe3',
+            }}>
+            <Feature coordinates={[coords.lng, coords.lat]} />
           </Layer>
         </Map>
         ;
