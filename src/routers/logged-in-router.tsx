557936b26f25a8c4d6c1d694f66115f03c3c9cbf
David Barcenas <daveepro@outlook.com>
2022-06-13 15:26:04 -0500

    add sidebar

--- src/routers/logged-in-router.tsx
+++ src/routers/logged-in-router.tsx
@@ -1,5 +1,4 @@
-import {useRoutes} from 'react-router-dom';
-import Header from '../components/header';
+import {Link, useRoutes} from 'react-router-dom';
 import Spinner from '../components/spinner';
 import {useProfile} from '../hooks/use-profile';
 import Error from '../pages/error';
@@ -22,10 +21,54 @@ function LoggedInRouter() {
   }
 
   return (
-    <>
-      <Header />
-      <div className='mx-auto w-full'>{router}</div>
-    </>
+    <div>
+      <div className='fixed top-0 left-0 h-full w-64 bg-gray-50'>
+        <div className='sidebar-header'>
+          <h1>Food Delivery</h1>
+        </div>
+        <div className='sidebar-body'>
+          <nav>
+            <ul>
+              <li>
+                <Link to='/'>
+                  <span className='material-icons-outlined'>home</span>
+                  <span className='sidebar-link'>Inicio</span>
+                </Link>
+              </li>
+              <li>
+                <Link to='/'>
+                  <span className='material-icons-outlined'>dashboard</span>
+                  <span className='sidebar-link'>Explorar</span>
+                </Link>
+              </li>
+              <li>
+                <Link to='/'>
+                  <span className='material-icons-outlined'>star_outline</span>
+                  <span className='sidebar-link'>Favoritos</span>
+                </Link>
+              </li>
+              <li>
+                <Link to='/'>
+                  <span className='material-icons-outlined'>email</span>
+                  <span className='sidebar-link'>Mensajes</span>
+                </Link>
+              </li>
+              <li>
+                <Link to='/'>
+                  <span className='material-icons-outlined'>settings</span>
+                  <span className='sidebar-link'>Ajustes</span>
+                </Link>
+              </li>
+            </ul>
+          </nav>
+          <button>
+            <span className='material-icons-outlined'>logout</span>
+            Cerrar sesión
+          </button>
+        </div>
+      </div>
+      <div className='main'>{router}</div>
+    </div>
   );
 }
 
